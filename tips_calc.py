d8a6a404c3fdecbba794b7d492f9d9447ef71e2d
EdemDevil <40803892+EdemDevil@users.noreply.github.com>
2023-01-16 12:29:55 +1000

    Update tips_calc.py

--- tips_calc.py
+++ tips_calc.py
@@ -1,6 +1,6 @@
 bill = float(input("Добро пожаловать в калькулятор чаевых\nКакой был общий счёт? "))
 percent = int(input("Какой процент вы бы оставили? 10, 12 или 15? "))
 total_person = int(input("Как много людей разделят счёт? "))
-tips = bill * (percent / 100) + 1 / total_person
+tips = bill * ((percent / 100) + 1) / total_person
 
 print(f"Каждый человек должен заплатить {round(tips, 2)} рублей")
